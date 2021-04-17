bee4d17b8e7da809c041f3d260f4ff4a3218e689
krishpranav <krisna.pranav@gmail.com>
2021-04-17 18:55:32 +0530

    updated searchcam.py

--- searchcam.py
+++ searchcam.py
@@ -12,3 +12,30 @@ def banner():
       print("\033[1;31m SEARCH CAM")
 
 banner()
+
+
+def http(keys, number):
+    
+    listing = []
+    for i in range(0, int(number), 1):
+        PAGE = i*10
+        URL = 'https://www.google.de/search'
+        PARAMETERS = {'q': str(keys), 'start': int(PAGE)}
+        r = requests.get(URL, params=PARAMETERS)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        parse_url = soup.findAll('div')
+        for _url in parse_url:
+            test = re.search(r"url\?q=(.+?)\&", str(_url))
+            if test is not None:
+                url = test.group(1)
+                listing.append(url)
+            else:
+                pass
+
+    url_list = (set(listing))
+    for captura in url_list:
+     print(captura)
+
+datos = open('cam.txt', 'r')
+for cam in datos:
+    http_(cam,5) #5 number page default 
