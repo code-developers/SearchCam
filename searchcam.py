f9444389f06f2db9ebc2d724876daf617918c9ee
krishpranav <krisna.pranav@gmail.com>
2021-04-17 18:52:01 +0530

    added banner

---
+++ searchcam.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env/python3
+#github: https://github.com/code-developers/SearchCam
+
+#imports
+import requests
+from bs4 import BeautifulSoup
+import re
+import colorama
+
+colorama.init()
+def banner():
+      print("\033[1;31m SEARCH CAM")
+
+banner()
